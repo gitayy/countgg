38328354da58036fc43d659109371a859622309a
pullgang <94886109+pullgang@users.noreply.github.com>
2023-10-30 22:39:17 -0700

    reli not relipaso

--- src/pages/AchievementsPage.tsx
+++ src/pages/AchievementsPage.tsx
@@ -77,7 +77,7 @@ import { UserContext } from '../utils/contexts/UserContext';
                 )
             })}
             </Grid>    
-            {!counter || (counter && counter.username !== 'Relipaso') && <><Typography sx={{mb: 1}} variant="h5">Secret Achievements</Typography>
+            {!counter || (counter && counter.username !== 'Reli') && <><Typography sx={{mb: 1}} variant="h5">Secret Achievements</Typography>
             <Grid container>
             {nonPublicAchievements.map((achievement) => {
               const counter_achievement = achievements && counter ? achievements.find((counterachievement) => {return counterachievement.achievementId === achievement.id && counterachievement.counterUUID === counter.uuid }) : undefined
